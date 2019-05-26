b96338de1815882c014bcde8986c71f0e348fcb1
Polariche <Polariche@gmail.com>
2019-05-27 02:26:23 +0900

    Bootstrap addition & Add search function

--- static/js/script.js
+++ static/js/script.js
@@ -1,82 +1,113 @@
-var box;
-var things;
+var show_num = 5;
+var fetch_num = 10;
 
-function link_html(json) {
-    //TODO: DOM 스타일로 바꾸기
-    return "<p id="+json.id+"> <a class='link' href="+ json.link + "> <img src="+json.favicon+" width='16px' height='16px'>" + json.title + "</a> <a onclick=\"remove("+json.id+", \'"+json.link+"\')\" href='#'> x </a> </p>";
+function link_obj(json) {
+    var p = $("<div></div>").attr("id", json.id).attr("class", 'btn-group');
+
+    var a = $("<button></button>").attr("type", "button").attr("class", "btn btn-primary link-btn").attr("onclick", "location.href = '"+json.link+"'");
+    p.append(a);
+
+    var t = $("<span>").attr("class", "d-inline-block text-truncate").attr("style","max-width: 250px;");
+    t.append($("<img>").attr("src", json.favicon).attr("class","link-icon"));
+    t.append(' ');
+    t.append(json.title);
+    a.append(t);
+
+    var x = $("<button></button>").attr("type", "button").attr("class", "btn btn-primary remove-btn").click(function() {remove(json.id);});
+    x.append('x');
+    p.append(x);
+
+    return p;
 }
 
-function fetch() {
-    things.html('');
-    $.post("/fetch", {}, function(data) {
+function error_msg(data) {
+    e = $("<div></div>").attr("role", "alert").html(data['message']);
+
+    if(data['status'] == 500)
+        e = e.attr("class", "alert alert-dark width-full text-center").html("INTERNAL ERROR");
+    else if(data['status'] == 400)
+        e = e.attr("class", "alert alert-danger width-full text-center").html("BAD REQUEST");
+    else
+        return;
+
+    $('#errorbox').append(e);
+    setTimeout(function(){e.remove();}, 3000);
+}
+
+function fetch(args) {
+    $("#things").html('');
+
+    return $.post("/fetch", args, function(data) {
         var n = data.length;
         var i = 0;
-        for(i=n-1;i>=0;i--) {
-            //latest link first
-            things.append(link_html(data[i]));
+        for(i=0;i<n;i++) {
+            var o = link_obj(data[n-1-i]);
+            $("#things").append(o);
+
+            if(i>=show_num)
+                o.hide();
         }
-    });
+        return data;
+    }).fail(function(data){error_msg(data)});
 };
 
 function onEnter(t) {
     // triggered when text inside the box is changed
+    
+    if (t=='') {
+        fetch({});  
+        return;
+    }
 
     // check if the text is url
     // copy-pasted from 
     // https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url
+
     var ex = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
     var regex = new RegExp(ex);
     if (t.match(regex)) {
-        box.val('');
+        $("#box").val('');
         save(t);
         return;
     }
 
+
     // check if the text is tag; if so, search by tag
 
     // if neither, search
-
-    $('#things').html(t);
+    fetch({title: t});
 }
 
-function search_title(t) {
-    
-}
-
-function search_tag(t) {
-
-}
-
-function render(indices) {
-    //render stuff
-}
 
 function save(t) {
     //send bookmark link to server
     //TODO: add tagging
 
     var link = t;
-    $.post("/save", {link: link}, function(data) {
+    return $.post("/save", {link: link}, function(data) {
         //add data
 
-        things.html(link_html(data)+things.html());
-    });
+        $("#things div:nth-child("+show_num+")").hide();
+        $("#things").prepend(link_obj(data));
+        
+        return data;
+    }).fail(function(data){error_msg(data)});
 };
 
-function remove(id, link) {
-    $('#'+id).remove();
-    $.post("/remove", {link: link}, function(data) {
-        
-    });
+function remove(id) {
+    return $.post("/remove", {id: id}, function(data) {
+
+        $('#'+id).remove();
+        $("#things div:nth-child("+show_num+")").show();
+
+        return data;
+    }).fail(function(data){error_msg(data)});
 }
 
 $(document).ready(function() {
-    box = $("#box");
-    things = $("#things");
-
-    box.change(function() {
-        onEnter(box.val());
+    $("#box").change(function() {
+        onEnter($("#box").val());
     });
 
-    fetch();
+    fetch({num: fetch_num});
 });
--- view.py
+++ view.py
@@ -1,7 +1,7 @@
 # import flask dependencies
 from flask import Flask, request, jsonify, render_template, send_from_directory, make_response
 import requests
-from requests.exceptions import Timeout
+from requests.exceptions import Timeout, ConnectionError
 import json
 import random
 import asyncio
@@ -21,7 +21,12 @@ def index():
 
 @app.route('/fetch', methods=['POST'])
 def fetch():
-    query = db.session.query(model.Link).all()
+    title = request.form.get('title')
+    query = db.session.query(model.Link)
+
+    if title != None:
+        query = query.filter((model.Link.title.contains(title))|(model.Link.link.contains(title)))
+
     return jsonify([x.json() for x in query])
 
 
@@ -48,13 +53,13 @@ def save():
     link = f"https://{corelink}{test.group(2)}"
 
     # Some of website doesn't even respond (or lack a title), so there has to be a default title
-    title_match = re.match(r'(?:[-a-zA-Z0-9]{2,256}\.)*([-a-zA-Z0-9]{2,256})(\.[a-z\.]{2,5})', corelink)
+    title_match = re.match(r'(?:[-a-zA-Z0-9]{2,256}\.)?([-a-zA-Z0-9]{2,256})(\.[a-z\.]{2,5})', corelink)
     title = title_match.group(1).title()
     if (len(title)<=2 or title_match.group(2) == '.io'):
         title = title + title_match.group(2)
 
     # Default values
-    o = {'link': link, 'title': title, 'favicon': 'https://'+corelink+'/favicon.ico'}
+    o = {'link': link, 'title': title, 'favicon': 'https://'+corelink+'/favicon.ico', 'message': ''}
 
     try:
         print(link)
@@ -64,7 +69,7 @@ def save():
         try:
             o['title'] = re.search('<title>(.*?)</title>', txt, re.IGNORECASE).group(1)
         except AttributeError:
-            print("Could not find title")
+            print("Could not find title; ")
 
         try:
             # higher quality
@@ -78,7 +83,14 @@ def save():
             print("No link rel='icon' ")
 
     except Timeout:
-        print("Timed Out")
+        msg = "Warning: Timed Out"
+        o['message'] = msg
+        print(msg)
+
+    except ConnectionError:
+        msg = "Warning: Couldn't connect"
+        o['message'] = msg
+        print(msg)
 
 
     # Save link data to DB
@@ -90,11 +102,10 @@ def save():
         db.session.commit()
         status = 201
         o = row.json()
-        o['message'] = 'Successfully Created!'
 
     except IntegrityError:
         status = 500
-        o['message'] = 'Link creation failed'
+        o['message'] = 'Failed to create link; Link already exists'
 
     o['status'] = status
 
@@ -103,16 +114,16 @@ def save():
 
 @app.route('/remove', methods=["POST"])
 def remove():
-    link = request.form.get('link')
-    x = model.Link.query.filter_by(link=link)
+    id_ = request.form.get('id')
+    x = model.Link.query.filter_by(id=id_)
 
     if not x:
-        return make_response(jsonify({'message': f'link {link} does not exist', 'status': 500}), 500)
+        return make_response(jsonify({'message': f'id {id_} does not exist', 'status': 500}), 500)
 
     x.delete()
     db.session.commit()
 
-    return make_response(jsonify({'message': f'link {link} removed successfully', 'status': 200}), 200)
+    return make_response(jsonify({'message': f'id {id_} removed successfully', 'status': 200}), 200)
 
 
 @app.route('/edit_title', methods=["POST"])
@@ -130,6 +141,11 @@ def edit_title():
 
     return make_response(jsonify({'message': f'link {link} modified successfully', 'status': 200}), 200)
 
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 """
